95cc4223387f873675ed6d6ad64e6f9b5d8c992d
Julien Damiano <julien.damiano@solution-libre.fr>
2024-04-10 12:12:34 +0200

    added layout for schedule

---
+++ themes/sdv/theme/layouts/schedule.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="bubble_container">
+        <div>
+            <div v-for="(item, index) in schedule" :key="index" class="bubble">
+                <div class="circle">{{ item.time }}</div>
+                <div class="bubble_content">
+                    <h1>{{ item.title }}</h1>
+                    <p>{{ item.description }}</p>
+                </div>
+            </div>
+        </div>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            schedule: [
+                { time: '9h00', title: 'Début du cours', description: '(Appel Pepal !)' },
+                { time: '10h30', title: 'Pause', description: '(15 minutes)' },
+                { time: '12h30', title: 'Pause Déjeuner', description: '(1 heure)' },
+                { time: '15h15', title: 'Pause', description: '(15 minutes)' },
+                { time: '17h00', title: 'Fin du cours', description: '' }
+            ]
+        };
+    }
+};
+</script>
+
+<style scoped>
+/* Ajoutez vos styles CSS ici si nécessaire */
+</style>
--- themes/sdv/theme/preludes/sdv.md
+++ themes/sdv/theme/preludes/sdv.md
@@ -2,8 +2,10 @@
 
 ## Organisation du cours
 
-📝 N'hésitez pas à prendre des notes, à réécrire les explications avec vos mots.
+-📝 N'hésitez pas à prendre des notes, à réécrire les explications avec vos mots.
 
-❓ Posez des questions 👍 Le but c'est que vous compreniez tout !
+-❓ Posez des questions 
 
-💯 A la fin, c'est noté !
+-👍 Le but c'est que vous compreniez tout !
+
+-💯 A la fin, c'est noté !
